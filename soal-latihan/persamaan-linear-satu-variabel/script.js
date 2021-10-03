60f116b28950cdcd14eb3602d984084f392c6cae
darrellhl082 <darrellhl082@gmail.com>
2021-10-03 20:02:38 +0700

    first commit

---
+++ soal-latihan/persamaan-linear-satu-variabel/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    x1 = ` jdkjdjkew`;
+    $('.x1').html(x1);
+    ans1 = `bismillah`;
+    $('#ans1').html(ans1);
+    x2 = ` jdkjdjkew`;
+    $('.x2').html(x2);
+    ans2 = `bismillah`;
+    $('#ans2').html(ans2);
+    x3 = ` jdkjdjkew`;
+    $('.x3').html(x3);
+    ans2 = `bismillah`;
+    $('#ans3').html(ans3);
+});
